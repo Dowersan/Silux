d26e54b10f4c0b0ed637e4d04396c197d0f8e8af
Chus <jesusmiguelaa@ufps.edu.co>
2019-08-23 18:58:00 -0500

    Segundo Commit Silux

--- Silux/app/urls.py
+++ Silux/app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index_view, name='index_view'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
